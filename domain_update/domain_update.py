666d82f1c9d4f086d3bae217854e4b2f26c19f7f
James Moseti <moseti.james4@gmail.com>
2024-01-06 23:34:38 +0300

    Updated the script to accept multiple kinds of old emails

--- domain_update/domain_update.py
+++ domain_update/domain_update.py
@@ -5,12 +5,14 @@ def update_domain(email, new_domain, old_domain):
         return new_email
     return email
 
-emails = ['james@outlook.com', 'marycook@outlook.com', 'joseph@outlook.com', 'janedoe@gmail.com']
+emails = ['james@outlook.com', 'marycook@yahoo.com', 'joseph@outlook.com', 'janedoe@gmail.com', 'johndoe@hotmail.com']
 new_emails = []
 
-for email in emails:
-    new_emails.append(update_domain(email, 'gmail.com', 'outlook.com'))
-
+old_domains = ['outlook.com', 'yahoo.com', 'hotmail.com', 'aol.com']
+for old_domain in old_domains:
+    for email in emails:
+        if old_domain in email:
+            new_emails.append(update_domain(email, 'gmail.com', old_domain))
 for email in new_emails:
     print(email)
 

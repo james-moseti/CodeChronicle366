bc597b8ae02dc86a2f7b874b845188cc672ec9e5
James Moseti <moseti.james4@gmail.com>
2024-01-21 21:11:48 +0300

    Added a simple plotting example.

---
+++ plotting_example/main.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+x_values = [1, 2, 3, 4]
+y_values = [5, 4, 6, 2 ]
+
+plt.plot(x_values, y_values)
+
+plt.show()
+
+plt.title("Sample plot")
+
+plt.xlabel("x values")
+
+plt.ylabel("y values")

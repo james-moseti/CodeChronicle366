0b6db66e95d99dd1a1063f697645da7731af7a1c
James Moseti <moseti.james4@gmail.com>
2025-10-30 23:50:16 +0300

    add linked list and dunder methods implementation

---
+++ dsa-py/linked-list/linked_list.py
@@ -0,0 +1,43 @@
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self): 
+        pass
+
+    def __contains__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def append(self, value):
+        pass
+
+    def prepend(self, value):
+        pass
+
+    def insert(self. value, index):
+        pass
+
+    def delete(self, value):
+        pass
+
+    def pop(self, index):
+        pass
+
+    def get(self, index):
+        pass
+
+    def print(self):
+        pass
+
+if __name__ == "__main__":
+    pass
---
+++ dunder-methods/simple_dunder.py
@@ -0,0 +1,12 @@
+# Dunder/Magic Methods in Python
+
+str1 = "Hello"
+str2 = "World"
+
+new_str = str1 + str2
+print(new_str)
+
+def func():
+    pass
+
+print(type(func))

bdbef79b1f6ac7ed678153fcd5ef575967bf9d7a
James Moseti <moseti.james4@gmail.com>
2025-11-01 23:58:34 +0300

    add dunder methods script

--- dunder-methods/simple_dunder.py
+++ dunder-methods/simple_dunder.py
@@ -1,12 +1,58 @@
 # Dunder/Magic Methods in Python
 
-str1 = "Hello"
-str2 = "World"
+class LinkedList:
+    def __init__(self):
+        self.items = []
 
-new_str = str1 + str2
-print(new_str)
+    # __repr__ gives an unambiguous string representation
+    def __repr__(self):
+        return f"LinkedList({self.items})"
 
-def func():
-    pass
+    # __str__ gives a user-friendly string
+    def __str__(self):
+        return " -> ".join(map(str, self.items)) or "Empty list"
 
-print(type(func))
+    # __len__ lets you use len(obj)
+    def __len__(self):
+        return len(self.items)
+
+    # __contains__ lets you use `in`
+    def __contains__(self, value):
+        return value in self.items
+
+    # __getitem__ enables indexing (obj[i])
+    def __getitem__(self, index):
+        return self.items[index]
+
+    # __setitem__ allows assigning (obj[i] = value)
+    def __setitem__(self, index, value):
+        self.items[index] = value
+
+    # __iter__ makes the object iterable (for loops)
+    def __iter__(self):
+        return iter(self.items)
+
+    # __eq__ enables == comparison
+    def __eq__(self, other):
+        if isinstance(other, LinkedList):
+            return self.items == other.items
+        return False
+
+    # custom add method to demonstrate updates
+    def add(self, value):
+        self.items.append(value)
+
+
+# Example usage
+lst = LinkedList()
+lst.add(10)
+lst.add(20)
+lst.add(30)
+
+print(lst)                  # Output: 10 -> 20 -> 30
+print(repr(lst))            # Output: LinkedList([10, 20, 30])
+print(len(lst))             # Output: 3
+print(20 in lst)            # Output: True
+print(lst[1])               # Output: 20
+lst[1] = 99
+print(lst)                  # Output: 10 -> 99 -> 30
